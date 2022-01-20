d0c08f6fa16537fc6fbb781f79762dc9dced6356
PalkeshCloudChamp <palkesh.goyal@blazeclan.com>
2022-01-20 23:29:55 +0530

    20-1-22

---
+++ src/main/java/com/formsExpress/FormsExpressApplication.java
@@ -0,0 +1,13 @@
+package com.formsExpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormsExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormsExpressApplication.class, args);
+	}
+
+}
